222c218f65dfab910a44ce549482876c34e4af63
Madhav Pathak <madhavpathak720@gmail.com>
2025-01-15 18:29:59 +0530

    Backend starter project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const express= require('express');
-
-const bodyParser=require('body-parser');
 const serverConfig = require('./config/dbConfig');
 const connectDB = require('./config/dbConfig');
 
@@ -10,10 +8,9 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended:'true'}));
 
-
 //procee.env to access environment variables
 app.listen(serverConfig.PORT || 5500,async()=>{
     await connectDB();
     console.log(`server started at Port ${serverConfig.PORT || 5500}..`);
     // console.log(process.env.PORT);
-}) 
+})
